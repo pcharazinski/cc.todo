2f499ab3011d40f710b44353be548178855a70a9
PatrycjaCiesla <47716020+PatrycjaCiesla@users.noreply.github.com>
2019-04-24 13:02:06 +0200

    + controller

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongodb');
-const express = require('express');
-
+const mongoose = require('mongoose');
 require('./models/db');
 
+const express = require('express');
+
+const todo_controller = require('./contollers/todo_controller');
 var app = express();
 
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log('Połączono do serwera na porcie '+ port);
 });
+
+
+app.use('./', todo_controller)
